84124a987ef1a9903523de8684873f4142873537
Timothy Sung <tsung8@gatech.edu>
2024-01-29 21:02:21 -0500

    added base mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import connectDB from './database/connect.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.get("/", (req, res) => {
 async function start() {
   const port = process.env.PORT;
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
