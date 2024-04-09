af89d0e7417bdc3bea22da3cb2ab871ce5321d35
ayang351 <ayang351@gatech.edu>
2024-04-09 17:54:58 -0400

    add icons and change design of toolbar

--- client/src/pages/Whiteboard.jsx
+++ client/src/pages/Whiteboard.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useMemo, useRef, useState} from 'react';
+import { FaEraser } from "react-icons/fa6";
+import { IconContext } from "react-icons";
+import { FaPencilAlt } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa";
+
+
 function Whiteboard() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
   const [isDrawing, setIsDrawing] = useState(false);
+
   const colors = useMemo(() => ["#FCA5A5", "#FDE047", "#93C5FD", "#86EFAC", "#000000"], [])
 
   useEffect(() => {
@@ -13,12 +20,14 @@ function Whiteboard() {
     canvas.style.width = `${window.innerWidth}px`;
     canvas.style.height = `${window.innerHeight}px`;
 
+    //reference for the canvas object
     const context = canvas.getContext("2d");
-    context.scale(2,2)
+    context.scale(2,2);
     context.lineCap = "round";
     context.strokeStyle = "black";
     context.lineWidth = 5;
     contextRef.current = context;
+
   }, [colors]);
 
   const startDrawing = ({nativeEvent}) => {
@@ -29,16 +38,16 @@ function Whiteboard() {
     contextRef.current.stroke();
     setIsDrawing(true);
     nativeEvent.preventDefault();
-
   };
+
 
   const draw = ({nativeEvent}) => {
     if(!isDrawing) {
       return;
     }
     const {offsetX, offsetY} =  nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY);
-    contextRef.current.stroke();
+      contextRef.current.lineTo(offsetX, offsetY);
+      contextRef.current.stroke();
     nativeEvent.preventDefault();
   };
 
@@ -72,7 +81,7 @@ function Whiteboard() {
   return (
     <>
     <div>
-      <h1 className ='text-xl font-bold text-center'>Whiteboard page</h1>
+      <h1 className ='text-xl font-bold text-center'>Whiteboard page</h1> 
       <canvas
       ref={canvasRef}
       onMouseDown={startDrawing}
@@ -80,40 +89,48 @@ function Whiteboard() {
       onMouseUp={stopDrawing}
       onMouseLeave={stopDrawing}>
       </canvas>
-    </div>
-    <div className='container flex justify-center items-center'>
-      <ul className="flex">
-        <li className='mr-6'>
-          <button className ="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+    <div className='flex justify-center py-4 bg-white sm: shadow-md md:shadow-lg lg: shadow-2xl rounded-full mx-3 my-5'>
+      <ul className="flex items-center">
+        <li className='mx-3'>
+          <button className ="bg-white hover:bg-gray-200 focus:bg-gray-200 text-gray-800 font-semibold  py-3 px-3 rounded-full"
           onClick={setToDraw}>
-            Pen
+            <IconContext.Provider value={{className: "shared-class", size:30}}> 
+          <FaPencilAlt/>
+          </IconContext.Provider>
           </button>
         </li>
-        <li className='mr-6'>
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        <li className='mx-3'>
+          <button className="bg-white hover:bg-gray-200 focus:bg-gray-200 text-gray-800 font-semibold  py-3 px-3 rounded-full"
           onClick={setToErase}>
-            Erase
+          <IconContext.Provider value={{ size:30}}> 
+          <FaEraser/>
+          </IconContext.Provider>
           </button>
       </li>
-      <li className='mr-6'>
-      <button onClick={() => setColor(colors[4])} className="bg-black py-4 px-4 w-4 h-4 rounded-full"> </button>
+      <li className='mx-3'>
+      <button onClick={() => setColor(colors[4])} className="bg-black py-4 px-4 w-5 h-5 rounded-full"> </button>
       </li>
-      <li className='mr-6'>
+      <li className='mx-3'>
       <button onClick={() => setColor(colors[0])} className="bg-red-300 py-4 px-4 w-4 h-4 rounded-full"> </button>
       </li>
-      <li className='mr-6'>
+      <li className='mx-3'>
       <button onClick={() => setColor(colors[1])} className="bg-yellow-300 py-4 px-4 w-4 h-4 rounded-full"> </button>
       </li>
-      <li className='mr-6'>
+      <li className='mx-3'>
       <button onClick={() => setColor(colors[2])} className="bg-blue-300 py-4 px-4 w-4 h-4 rounded-full"> </button>
       </li>
-      <li className='mr-6'>
+      <li className='mx-3'>
       <button onClick={() => setColor(colors[3])} className="bg-green-300 py-4 px-4 w-4 h-4 rounded-full"> </button>
       </li>
-      <li className='mr-6'>
-      <a id="download_image_link" href="download_link" onClick={saveImageToLocal} className='text-gray-900 underline'> export as image </a>
+      <li className='mx-6'>
+      <a id="download_image_link" href="download_link" onClick={saveImageToLocal} className='text-gray-800'> 
+      <IconContext.Provider value={{size:30}}> 
+          <FaDownload/> 
+          </IconContext.Provider>
+      </a>
       </li>
       </ul>
+    </div>
     </div>
     </>
   )
